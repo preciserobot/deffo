415e583185c9aec39141bc35b01988fcc0cb7ca3
David Brawand <dbrawand@nhs.net>
2017-08-22 16:18:49 +0100

    bugfixes

--- mac/conviceApple.sh
+++ mac/conviceApple.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-VMNAME= ${0}
-
+VMNAME=$1
+echo $VMNAME
 VBoxManage modifyvm $VMNAME --cpuidset 00000001 000306a9 04100800 7fbae3ff bfebfbff
 VBoxManage setextradata $VMNAME "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "MacBookPro11,3"
 VBoxManage setextradata $VMNAME "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
